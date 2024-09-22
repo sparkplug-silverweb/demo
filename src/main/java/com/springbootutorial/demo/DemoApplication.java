9078c5bd2cc091c4f04f56562d5fae1b17261851
Lester Serdenia <lester.serdenia@accenture.com>
2024-09-22 18:01:10 +0800

    Initial Commit

---
+++ src/main/java/com/springbootutorial/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.springbootutorial.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+// REFERENCE: https://dev.to/dewanahmed/create-a-spring-boot-application-with-a-mysql-backend-1bl2
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
